a2d7df7ebdaeef05135f8f24afe089f86ea15b09
Moyinnnn <161252443+Moyinnnn@users.noreply.github.com>
2024-07-11 04:52:25 +0100

    Added PHP file

---
+++ userinfo.php
@@ -0,0 +1,9 @@
+<?php
+// displays information on client (name and email)
+$name = $_POST["Leanne Graham"];
+$email = $_POST["Sincere@april.biz"];
+
+// provides information on the program
+$servername = "127.0. 0.1";
+$prgrmr_name = "Mofiyinfoluwa";
+$date = "10/07/2024";
